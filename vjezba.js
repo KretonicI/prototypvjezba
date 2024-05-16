43bf123070f276b679a4195ba66fe3d14a30576b
office10 <office10@ucione.local>
2024-05-16 17:22:09 +0200

    dodaj

--- vjezba.js
+++ vjezba.js
@@ -12,3 +12,17 @@ console.log("Ime psa default name = " + dog.dogName);
 
 var dog1 = new Dog ("Bobi");
 console.log("Ime psa = " + dog1.dogName);
+
+Dog.prototype.imePsa = function (){
+    return this.dogName;
+
+};
+
+Dog.prototype.bark = function (){
+    return "wuf wuf";
+
+};
+console.log("Ime psa (dog) preko metode " + dog.imePsa());
+console.log("Imew psa2 (dog2) preko metode " + dog1.imePsa());
+console.log("Pas1 se glasa = " + dog.bark());
+console.log("Pas2 se glasa = " + dog1.bark());
